88935ce417cfa5d68846eb0bf688c7ae011059c9
CarlCajado <91168900+CarlCajado@users.noreply.github.com>
2021-10-01 20:49:20 -0300

    Create InterfTest.java

---
+++ InterfTest.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class InterfTest {
+	public static void main(String[] args)
+	 {
+	 Interf panel = new Interf();
+	 JFrame application = new JFrame();
+	
+	 application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 application.add(panel);
+	 application.setSize(230, 250);
+	 application.setVisible(true);
+	 }
+
+}
